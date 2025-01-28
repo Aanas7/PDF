87fe1580479e81b6ffb90c811fb9f54aa18f27cf
Anas Ansari <145539006+Aanas7@users.noreply.github.com>
2025-01-28 20:53:01 +0500

    Add files via upload

---
+++ Javascript Assignment/Chap5/app.js
@@ -0,0 +1,38 @@
+// // // var num1 = 5;
+// // // var num2 = 3;
+
+// // // var totalNum = num1 + num2;
+// // // // var totalNum = num1 - num2;
+// // // // var totalNum = num1 * num2;
+// // // // var totalNum = num1 / num2;
+// // // // var totalNum = num1 % num2;
+
+// // // document.write(totalNum);
+
+
+// // var num1 = undefined;
+// // var num2 = 5
+// // var num3 = num2 + 1;
+// // var num4 = num3 + 7;
+// // var num5 = num4 - 1;
+// // var num6 = num5 % 3;
+
+// // document.write("Value after variable is" + " " +  num1 , "<br/>");
+// // document.write("Initial value is" + " " + num2 , "<br/>")
+// // document.write("Value after increament is" + " " + num3 , "<br/>");
+// // document.write("Value after addition is" + " " + num4 , "<br/>");
+// // document.write("Value after decreament is" + " " + num5 , "<br/>");
+// // document.write("The remainder is:" + " " + num6);
+
+
+
+// var cost = 600;
+// var totalCost = cost * 5;
+
+// document.write("Total cost to buy 5 tickets to a movie is" + " " + totalCost + "PKR");
+
+
+for (var i = 1; i <= 10; i++) {
+    // document.write("2 x 1 = 2" + "<br />")
+    document.write("7 x" + i + "=" + 7 * i + "<br />");
+}
